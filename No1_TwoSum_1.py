d52e8b8ac8cec686bdb575747685b0c8ee83c3f2
firiceguo <firiceguo@yahoo.com>
2016-08-25 23:03:01 +0800

    No1_1

---
+++ No1_TwoSum_1.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    # O(n^2) Version
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for x in xrange(0, len(nums) - 1):
+            for y in xrange(x + 1, len(nums)):
+                if nums[x] + nums[y] == target:
+                    return([x, y])
+
+    def readNums(self):
+        st = raw_input()
+        nums = []
+        for s in st:
+            try:
+                n = int(s)
+                nums.append(n)
+            except Exception:
+                pass
+        return(nums)
+
+    def readTarget(self):
+        return(int(raw_input()))
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = sol.readNums()
+    target = sol.readTarget()
+    print sol.twoSum(nums, target)
---
+++ No1_TwoSum_2.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    # O(n) Version from others
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        tmp = []
+        for i in range(len(nums)):
+            if target - nums[i] in tmp:
+                return(tmp[target - nums[i]], i)
+            else:
+                tmp[nums[i]] = i
+
+    def readNums(self):
+        st = raw_input()
+        nums = []
+        for s in st:
+            try:
+                n = int(s)
+                nums.append(n)
+            except Exception:
+                pass
+        return(nums)
+
+    def readTarget(self):
+        return(int(raw_input()))
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = sol.readNums()
+    target = sol.readTarget()
+    print sol.twoSum(nums, target)
